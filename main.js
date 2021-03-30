662c2d4db5746fd2827c25dab9c30a24b81cc7b0
IvanShyvanau <kippbelarus@gmail.com>
2021-03-30 22:10:06 +0300

    finish

---
+++ main.js
@@ -0,0 +1,47 @@
+
+
+
+/*
+var a = 3;
+var b = 1;
+var c = 2;
+console.log((a + b) / 2);
+
+a = 5;
+!!a > true
+0 Nan > false
+
+if (3 > 1) {
+  console.log(2);
+}
+if (3 < 1) {
+  console.log(3);
+}
+console.log(4);
+
+var a = 52;
+if (a >= 18 && a <= 21){
+  console.log("Привет");
+}else if (a > 21) { 
+  console.log("здарова");
+}else{
+  console.log("здравствуйте");
+}
+
+a > 18 ? console.log("Привет"):console.log("buy");*/
+
+var a = 3;
+var b = 4;
+if (a > b){
+  console.log(a + b);
+}else{
+  a = a + b;
+  console.log(a);
+}
+if (a > b){
+  console.log(a + b);
+}else{
+  console.log(a - b);
+}
+a > b? console.log(a+ b) : console.log(a+ b);
+
